7d1f79ab13d5dda9a8874079384e942f5e73bf05
Lumen <lumen.gnu@gmail.com>
2026-06-03 12:15:06 +0300

    test suite

--- dev-scripts/run-tests.bash
+++ dev-scripts/run-tests.bash
@@ -37,19 +37,31 @@ echo -e "\n\e[34;1m▶ Running Tests:\e[0m [$(date +'%Y-%m-%d %H:%M:%S')]" >&2
 echo -e "config: \e[1m${CONFIG_FILE}\e[0m  tests: \e[1m${TESTS}\e[0m" >&2
 # Безопасное получение ширины терминала (фоллбэк на 80 для CI/CD)
 TERM_COLS=$(tput cols 2>/dev/null || echo 80)
-printf '%*s\n' "$(($TERM_COLS - 5))" '' | tr ' ' '~' >&2
+printf '%*s\n\n' "$(($TERM_COLS - 5))" '' | tr ' ' '~' >&2
 
 npx vscode-test --config "${CONFIG_FILE}" 2>&1 |
-    awk '{
-            if (match($0, /Exit code:[[:space:]]*[0-9]+/)) {
+    awk '
+        EGIN { failed = 0; total = 0 }
+        {
+            if (match($0, /^Exit code:[[:space:]]*[0-9]+/)) {
                 code = substr($0, RSTART, RLENGTH)
                 num = code; sub(/Exit code:[[:space:]]*/, "", num)
                 color = (num == "0") ? "\033[32;1m" : "\033[31;1m"
                 print substr($0, 1, RSTART-1) color code "\033[0m" substr($0, RSTART+RLENGTH)
+                print " "
+                total++
+                if (num + 0 != 0) failed++
             } else if (match($0, /[0-9]+ tests? failed/)) {
                 print substr($0, 1, RSTART-1) "\033[31;1m" substr($0, RSTART, RLENGTH) "\033[0m" substr($0, RSTART+RLENGTH)
             } else { print }
-        }' || { echo -e "\n\e[31;1m[FAIL] Test run failed\e[0m\n" >&2 ; exit 1 ; }
+        }
+        END {
+            if (total > 0) {
+                color = (failed == 0) ? "\033[32;1m" : "\033[31;1m"
+                print color "Runner: " (total - failed) "/" total " passed\033[0m" > "/dev/stderr"
+            }
+        }
+        ' || { echo -e "\n\e[31;1m[FAIL] Test run failed\e[0m\n" >&2 ; exit 1 ; }
 
 echo -e "\n\e[32;1m[DONE] All tests passed\e[0m\n" >&2
 exit 0
